dfc7687dc31207303b71b0a5ab4b4305996956cb
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-06-27 14:26:13 +0530

    Updated

--- DSA/src/recursion/BinarySearch.java
+++ DSA/src/recursion/BinarySearch.java
@@ -2,15 +2,18 @@ package recursion;
 
 public class BinarySearch {
 	public static void main(String[] args) {
-		int[] arr= {12,23,34,45,56,67,78,89,90};
+		int[] arr= {1,2,3,4,5,6,7};
 		int key=90;
 		System.out.println(binSearch(arr, key, 0, arr.length-1));
 	}
 	public static int binSearch(int[] arr,int key,int start,int end) {
+		
 		if(start>end) {
 			return -1;
 		}
+		
 		int mid=start+(end-start)/2;
+		System.out.println(mid);
 		if(arr[mid]==key) {
 			return mid;
 		}
