f9e36de4afa3521c4a36ac59f57f23538958af0c
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-06-27 15:27:46 +0530

    Updated

--- DSA/src/com/DSA/logivalproblems/DeciToBin.java
+++ DSA/src/com/DSA/logivalproblems/DeciToBin.java
@@ -12,8 +12,23 @@ public class DeciToBin {
 		}
 		return bin;
 	}
+	
+	public static Long binToDec(Long bin) {
+		int c =0;
+		int res = (int) (bin%10);
+		Long dec = 0l;
+		while(bin>0) {
+			if(res==1) {
+			dec+=(int)Math.pow(2, c);
+			}
+			c++;
+			bin/=10;
+		}
+		return dec;
+	}
 
 	public static void main(String[] args) {
-		System.out.println(decToBin(4));
+		System.out.println(decToBin(7));
+		System.out.println(binToDec(111l));
 	}
 }
