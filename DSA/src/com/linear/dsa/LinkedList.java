c881d51b4ef6c755aa27fdcfb5d9285c9363afcb
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-07-10 12:47:48 +0530

    Updated

--- DSA/src/com/linear/dsa/LinkedList.java
+++ DSA/src/com/linear/dsa/LinkedList.java
@@ -53,14 +53,52 @@ public class LinkedList {
 			System.out.println();
 		}
 	}
+	
+	public int size() {
+		int count =0;
+		if(head == null) {
+			return count;
+		}else {
+			Node temp = head;
+			while(temp!=null) {
+				temp=temp.next;
+				count++;
+			}
+			return count;
+		}
+	}
+	
+	public void insertMiddle(int index,int value) {
+		if(index>size() || index<0) {
+			System.out.println("Invalid Index....");
+		}
+		else if(index==0) {
+			insertFirst(value);
+		}
+		else if(index==size()) {
+			insert(value);
+		}
+		else {
+			int count=0;
+			Node temp = head;
+			Node newNode = new Node(value);
+			while(count<index-1) {
+				count++;
+				temp=temp.next;
+			}
+			newNode.next=temp.next;
+			temp.next=newNode;
+		}
+	}
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.insert(12);
 		list.insert(23);
 		list.insert(13);
 		list.insert(14);
-		list.display();
 		list.insertFirst(10);
+		list.insertMiddle(4, 244);
 		list.display();
+		System.out.println("Size of list: "+list.size());
 	}
 }
