63f18093297541737b771bf5c2461d863218ca17
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-07-13 15:37:00 +0530

    Updated

---
+++ DSA/src/com/leetcode/dsa/RevNumber.java
@@ -0,0 +1,12 @@
+package com.leetcode.dsa;
+
+public class RevNumber {
+	static int revNumber(int number) {
+		int no = number;
+		while(no)
+	}
+	public static void main(String[] args) {
+		int number=-123;
+	
+	}
+}
--- DSA/src/recursion/RemoveAInString.java
+++ DSA/src/recursion/RemoveAInString.java
@@ -13,9 +13,22 @@ public class RemoveAInString {
 			}
 		}
 	}
+	
+	static String removeA(String str) {
+		if(str.isEmpty()) {
+			return "";
+		}
+		char ch = str.charAt(0);
+		if(ch=='a') {
+			return removeA(str.substring(1));
+		}
+		else {
+			return ch+removeA(str.substring(1));
+		}
+	}
 	public static void main(String[] args) {
 		String str = "baccab";
 		System.out.println(removeA(str,""));
-//		System.out.println(str.substring(1));
+		System.out.println(removeA(str));
 	}
 }
