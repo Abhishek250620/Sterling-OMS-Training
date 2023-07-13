e683f66b92783d40a2d63e37b983ab266658c247
Abhishek M R <84577390+Abhishek250620@users.noreply.github.com>
2023-07-13 17:21:12 +0530

    Updated

--- DSA/src/com/leetcode/dsa/SubSets.java
+++ DSA/src/com/leetcode/dsa/SubSets.java
@@ -1,5 +1,8 @@
 package com.leetcode.dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubSets {
 	
 	static void subSet(String str,String sub) {
@@ -13,8 +16,22 @@ public class SubSets {
 		subSet(str.substring(1),sub);
 	}
 	
+	static List<String> subSet(String str) {
+		List<String> list = new ArrayList<String>();
+		list.add(" ");
+		for(int i =0;i<str.length();i++) {
+			String sub="";
+			for(int j=i;j<str.length();j++) {
+				sub+=str.charAt(j);
+				list.add(sub);
+			}
+		}
+		return list;
+	}
+	
 	public static void main(String[] args) {
 		String str="abc";
 		subSet(str,"");
+		System.out.println(subSet(str));
 	}
 }
